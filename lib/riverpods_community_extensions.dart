46b1ac96ca001a16b443ac4029324ae5628a91b2
Clément Taboulot <clement-taboulot@hotmail.fr>
2023-11-08 14:04:41 +0100

    Init with very good cli

---
+++ lib/riverpods_community_extensions.dart
@@ -0,0 +1,4 @@
+/// Community extensions for Riverpods
+library riverpods_community_extensions;
+
+export 'src/riverpods_community_extensions.dart';
---
+++ lib/src/riverpods_community_extensions.dart
@@ -0,0 +1,7 @@
+/// {@template riverpods_community_extensions}
+/// Community extensions for Riverpods
+/// {@endtemplate}
+class RiverpodsCommunityExtensions {
+  /// {@macro riverpods_community_extensions}
+  const RiverpodsCommunityExtensions();
+}
